9cb892af4464067548488745848eb1df52022fb3
Wes Hargrove <wesleyhargrove@gmail.com>
2015-03-05 13:18:01 -0800

    Convert tabs to spaces

--- 001_multiples_of_3_and_5/multiples_of_3_and_5.rb
+++ 001_multiples_of_3_and_5/multiples_of_3_and_5.rb
@@ -1,9 +1,9 @@
 def sum_of_multiples(integer)
-	sum = 0
-	(0...integer).each do |i|
-		if i % 3 == 0 || i % 5 == 0
-			sum += i
-		end
-	end
-	return sum
+  sum = 0
+  (0...integer).each do |i|
+    if i % 3 == 0 || i % 5 == 0
+      sum += i
+    end
+  end
+  return sum
 end
